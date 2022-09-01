06fe4f79777f10905253ec8d22c2a790da6c27c0
Chez92 <alexmconnor92@gmail.com>
2022-09-01 13:59:37 +0100

    AC - Fixed StackOverflow exception in Npc ShouldDropItem method and copied logic from Player class as it looks sensible.

--- Intersect.Server/Entities/Npc.cs
+++ Intersect.Server/Entities/Npc.cs
@@ -181,8 +181,23 @@ namespace Intersect.Server.Entities
 
         protected override bool ShouldDropItem(Entity killer, ItemBase itemDescriptor, Item item, float dropRateModifier, out Guid lootOwner)
         {
-            lootOwner = (killer as Player)?.Id ?? Id;
-            return ShouldDropItem(killer, itemDescriptor, item, dropRateModifier, out _);
+            lootOwner = default;
+
+            if (killer?.GetEntityType() == EntityTypes.Player)
+            {
+                var dropRate = item?.DropChance * 1000 * dropRateModifier;
+                var dropResult = Randomization.Next(1, 100001);
+                if (dropResult >= dropRate)
+                {
+                    return false;
+                }
+
+                // Set the attributes for this item.
+                item.Set(new Item(item.ItemId, item.Quantity, true));
+                return true;
+            }
+
+            return false;
         }
 
         public bool TargetHasStealth(Entity target)
